0cd4c9eed2d7f44693bc4ddcc6e7b760ded2a111
JasonLuis <jason20luis@gmail.com>
2023-03-15 20:29:21 -0300

    chore: create project rest-countries-api and add quasar

---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+import { quasar } from '@quasar/vite-plugin';
+import { defineNuxtConfig } from 'nuxt/config';
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  srcDir: 'src/',
+  dir: {
+    pages: 'pages',
+    assets: 'core/assets',
+    layouts: 'core/layouts',
+    public: 'core/public'
+  },
+  components: {
+    dirs: ['core/components']
+  },
+  css: [
+    '@quasar/extras/roboto-font/roboto-font.css',
+    '@quasar/extras/material-icons/material-icons.css',
+    '~/core/assets/styles/quasar.sass'
+  ],
+  build: {
+    transpile: ['quasar']
+  },
+  plugins: ['~/core/plugins/quasar.client.ts'],
+  imports: {
+    dirs: ['~/core/composables/**']
+  },
+  vite: {
+    plugins: [
+      /* vue({
+              template: { transformAssetUrls }
+            }), */
+      quasar({
+        sassVariables: '~/core/assets/styles/quasar.variables.sass'
+      })
+    ]
+  }
+});
