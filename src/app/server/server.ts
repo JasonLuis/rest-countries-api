3a69e996bb2c33fcc099599c2b3650f1940023ce
JasonLuis <jason20luis@gmail.com>
2025-07-13 04:31:25 -0300

    feat: cria metodo getCountryByName e corrige metodo filterCountriesByName

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -1,3 +1,5 @@
+import { CountryDto } from "../model/ICountry";
+
 export class ServerAPI {
   async getAllCountrys(): Promise<Array<any>> {
     const path = new URL(`./data.json`, import.meta.url).href;
@@ -38,7 +40,41 @@ export class ServerAPI {
       return res;
     }
 
-    const filter = res.filter(item => item.name.includes(country));
+    const filter = res.filter(item => item.name.toLowerCase().includes(country.toLowerCase()));
+
+    return filter;
+  }
+
+  async getCountryByName(country: string): Promise<CountryDto.ICountry> {
+    const path = new URL(`./data.json`, import.meta.url).href;
+    const data = await fetch(path);
+    const res = await data.json();
+
+    if (!Array.isArray(res)) {
+      throw new TypeError('O arquivo JSON não existe');
+    }
+
+    const countries: Array<CountryDto.ICountry> = res.map(item => {
+      return {
+        name: item.name as string,
+        nativeName: item.nativeName as string,
+        population: item.population as number,
+        region: item.region as string,
+        subregion: item.subregion as string,
+        capital: item.capital as string,
+        topLevelDomain: item.topLevelDomain as string[],
+        currencies: item.currencies as CountryDto.Currency[],
+        languages: item.languages as CountryDto.Language[],
+        borders: item.borders as string[],
+        flag: item.flag as string,
+      };
+    });
+
+    const filter = countries.find(item => item.name.toLowerCase() === country.toLowerCase());
+
+    if (!filter) {
+      throw new Error('País não encontrado');
+    }
 
     return filter;
   }
