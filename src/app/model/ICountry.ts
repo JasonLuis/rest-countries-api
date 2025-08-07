6bde28d2d215e8f58224f3a60e9a9b28458b378a
JasonLuis <jason20luis@gmail.com>
2025-08-07 19:55:31 -0300

    fix: changed population item for type string

--- src/app/model/ICountry.ts
+++ src/app/model/ICountry.ts
@@ -1,25 +1,24 @@
 export namespace CountryDto {
-    export interface ICountry {
-        name: string;
-        nativeName: string;
-        population: number;
-        region: string;
-        subregion: string;
-        capital: string;
-        topLevelDomain: Array<string>;
-        currencies: Array<Currency>;
-        languages: Array<Language>;
-        borders: Array<string>;
-        flag: string;
-    }
+  export interface Currency {
+    name: string;
+    symbol: string;
+  }
 
-    export interface Currency {
-        name: string;
-        symbol: string;
-    }
+  export interface Language {
+    name: string;
+  }
 
-    export interface Language {
-        name: string;
-    }
+  export interface ICountry {
+    name: string;
+    nativeName: string;
+    population: string;
+    region: string;
+    subregion: string;
+    capital: string;
+    topLevelDomain: Array<string>;
+    currencies: Array<Currency>;
+    languages: Array<Language>;
+    borders: Array<string>;
+    flag: string;
+  }
 }
-
